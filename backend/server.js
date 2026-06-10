a1559a04a607d3881dc6090fdc9030f517a1d3de
sucimituhu-prog <sucimituhu@gmail.com>
2026-06-10 09:03:57 +0700

    fix: tambah vercel.json dan update CORS

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const path    = require("path");
 const app     = express();
 const PORT    = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // Serve folder uploads/
